caba0f5d4c9a3c12e9716fc5a9c74b4421544641
Momen1995 <momensrk@gmail.com>
2024-09-21 19:32:16 +0600

    data show on table add

--- src/components/RecentTransactions/RecentTransactions.jsx
+++ src/components/RecentTransactions/RecentTransactions.jsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
+import { BalanceContext } from "../Context/BalanceContext";
+import Transaction from "./Transaction";
+
 const RecentTransactions = () => {
+  const { userDetail} = useContext(BalanceContext);
+
   return (
-    <div className="bg-[#fff] w-3/5 shadow-md rounded">
+    <div className="bg-[#fff] w-3/5 shadow-md rounded overflow-y-scroll">
       <h3 className="bg-[#f16510] text-white mb-3 h-14 p-4 text-left text-xl">Recent Transactions</h3>
+      <div className="p-4">
+        {
+          userDetail.map(transaction => <Transaction key={transaction.key} transaction={transaction}/>)
+        }
+      </div>
     </div>
   );
 };
---
+++ src/components/RecentTransactions/Transaction.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FaPlus, FaMinus } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+
+const Transaction = ({ transaction }) => {
+  const {description,amount,type} = transaction;
+  
+  return (
+    <div className="flex justify-between items-center p-4 border-b-2">
+      <div className="flex gap-3 items-center">
+        <h1 className="text-red-700 text-xl">
+          {type === "income" ? <FaPlus /> : <FaMinus />}
+        </h1>
+        <h1 className="text-xl font-semibold text-gray-600">{description}</h1>
+      </div>
+      <div className="flex gap-8 items-center justify-center">
+        <p className="text-xl font-semibold text-gray-600">{amount}</p>
+        <p className="text-[22px] font-semibold text-teal-700">
+          <MdDelete />
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Transaction;
